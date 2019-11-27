cfa2154db08ad014ecd7946f7c4a6a23634d4452
nishimoto <a32b16c4@gmail.com>
2019-11-28 00:29:07 +0900

    es module

--- dist/hello.js
+++ dist/hello.js
@@ -1,22 +1,10 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+export var Hello;
+(function (Hello) {
+    function hello() {
+        const p = document.createElement("button");
+        p.textContent = "hello";
+        document.body.appendChild(p);
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var Hello;
-    (function (Hello) {
-        function hello() {
-            const p = document.createElement("button");
-            p.textContent = "hello";
-            document.body.appendChild(p);
-        }
-        Hello.hello = hello;
-    })(Hello = exports.Hello || (exports.Hello = {}));
-});
+    Hello.hello = hello;
+})(Hello || (Hello = {}));
 //# sourceMappingURL=hello.js.map
